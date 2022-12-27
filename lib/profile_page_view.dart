84a9d0540037a0b06f76956f3b710c2b2007ad9d
HappyNathani <happynathani2003@gmail.com>
2022-12-27 18:34:49 +0530

    ADD: validate form

--- lib/profile_page_view.dart
+++ lib/profile_page_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ProfilePageView extends StatelessWidget {
   const ProfilePageView({super.key, required this.profileData});
@@ -13,7 +14,73 @@ class ProfilePageView extends StatelessWidget {
           "Profile Page",
         ),
       ),
-      body: Text(profileData.toString()),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 30,
+            ),
+            Container(
+              height: 150,
+              width: 150,
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle, color: Colors.blue[100]),
+              child: const Icon(
+                Icons.person,
+                size: 100,
+                color: Colors.blue,
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Text(
+              "Profile",
+              style: GoogleFonts.libreBaskerville(
+                fontSize: 30,
+                color: Colors.blue,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+              height: 200,
+              width: MediaQuery.of(context).size.width,
+              margin: const EdgeInsets.all(25),
+              padding: const EdgeInsets.all(30),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.blue[100],
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    "Name: ${profileData["name"]}",
+                    style: const TextStyle(fontSize: 18),
+                  ),
+                  const Divider(
+                    color: Colors.blue,
+                  ),
+                  Text(
+                    "Mobile No: ${profileData["mobileNumber"]}",
+                    style: const TextStyle(fontSize: 18),
+                  ),
+                  const Divider(color: Colors.blue),
+                  Flexible(
+                    child: Text(
+                      "Email: ${profileData["email"]}",
+                      style: const TextStyle(fontSize: 18),
+                    ),
+                  ),
+                  const Divider(color: Colors.blue),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/signup_page_view.dart
+++ lib/signup_page_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:form/profile_page_view.dart';
 
 class SignupPageView extends StatefulWidget {
@@ -14,6 +15,8 @@ class _SignupPageViewState extends State<SignupPageView> {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
   TextEditingController confirmPasswordController = TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,83 +28,146 @@ class _SignupPageViewState extends State<SignupPageView> {
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16),
           child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                TextField(
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: "Name",
+            child: Form(
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: "Name",
+                    ),
+                    keyboardType: TextInputType.name,
+                    controller: nameController,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter Name';
+                      }
+                      return null;
+                    },
                   ),
-                  keyboardType: TextInputType.name,
-                  controller: nameController,
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                TextField(
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: "Mobile Number",
+                  const SizedBox(
+                    height: 16,
                   ),
-                  keyboardType: TextInputType.number,
-                  controller: mobileNumberController,
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                TextField(
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: "Email",
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: "Mobile Number",
+                    ),
+                    inputFormatters: [
+                      FilteringTextInputFormatter.digitsOnly,
+                      LengthLimitingTextInputFormatter(10),
+                    ],
+                    validator: (value) {
+                      if (value == null ||
+                          value.isEmpty ||
+                          value.length != 10) {
+                        return 'Please enter valid Mobile Number';
+                      }
+                      return null;
+                    },
+                    keyboardType: TextInputType.number,
+                    controller: mobileNumberController,
                   ),
-                  keyboardType: TextInputType.emailAddress,
-                  controller: emailController,
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                TextField(
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: "Password",
+                  const SizedBox(
+                    height: 16,
                   ),
-                  obscureText: true,
-                  keyboardType: TextInputType.visiblePassword,
-                  controller: passwordController,
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                TextField(
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: "Confirm Password",
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: "Email",
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                    controller: emailController,
+                    validator: (value) {
+                      String p =
+                          r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$";
+                      RegExp regExp = RegExp(p);
+                      bool isEmail = regExp.hasMatch(value ?? '');
+
+                      if (value == null || value.isEmpty || !isEmail) {
+                        return 'Please enter valid Email';
+                      }
+
+                      return null;
+                    },
                   ),
-                  obscureText: true,
-                  keyboardType: TextInputType.visiblePassword,
-                  controller: confirmPasswordController,
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                ElevatedButton.icon(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => ProfilePageView(profileData: {
-                          "name": nameController.text,
-                          "mobileNumber": mobileNumberController.text,
-                          "email": emailController.text,
-                        }),
-                      ),
-                    );
-                  },
-                  icon: const Icon(Icons.keyboard_arrow_right),
-                  label: const Text("Sign Up"),
-                ),
-              ],
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: "Password",
+                    ),
+                    obscureText: true,
+                    keyboardType: TextInputType.visiblePassword,
+                    controller: passwordController,
+                    validator: (value) {
+                      bool hasCapital = RegExp(r"[A-Z]").hasMatch(value ?? '');
+                      bool hasSmall = RegExp(r"[a-z]").hasMatch(value ?? '');
+                      bool hasNamber = RegExp(r"[0-9]").hasMatch(value ?? '');
+                      bool hasSpecialChar =
+                          RegExp(r"^[_\-=@,\.;]+$").hasMatch(value ?? '');
+
+                      if (value == null ||
+                          value.isEmpty ||
+                          !(hasCapital && hasSmall && hasNamber)) {
+                        return 'Please enter Strong Password';
+                      }
+
+                      return null;
+                    },
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: "Confirm Password",
+                    ),
+                    obscureText: true,
+                    keyboardType: TextInputType.visiblePassword,
+                    controller: confirmPasswordController,
+                    validator: (value) {
+                      if (value == null ||
+                          value.isEmpty ||
+                          value != passwordController.text) {
+                        return 'Please check confirm password';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      if (_formKey.currentState?.validate() == true) {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ProfilePageView(
+                              profileData: {
+                                "name": nameController.text,
+                                "mobileNumber": mobileNumberController.text,
+                                "email": emailController.text,
+                              },
+                            ),
+                          ),
+                        ).then((value) {
+                          _formKey.currentState?.reset();
+                        });
+                      }
+                    },
+                    icon: const Icon(Icons.keyboard_arrow_right),
+                    label: const Text("Sign Up"),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
